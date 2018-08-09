575af1c2b9da123e67ab92bffa9d2e0b58cf6f00
Amy <amyadzuki@gmail.com>
2018-08-09 16:35:44 +0000

    Bugfixes

--- handlers/http.go
+++ handlers/http.go
@@ -18,11 +18,11 @@ func Http(ctx *fasthttp.RequestCtx) {
 }
 
 func http(ctx *fasthttp.RequestCtx) {
-	if match, tail := str.CaseHasPrefix(ctx.Path(), "/api"); match {
+	if match, tail := str.CaseHasPrefix(string(ctx.Path()), "/api"); match {
 		Api(ctx, tail)
 	}
 	var uri fasthttp.URI
-	ctx.URI.CopyTo(&uri)
+	ctx.URI().CopyTo(&uri)
 	uri.SetScheme("https")
 	ctx.RedirectBytes(uri.FullURI(), 301) // 301 recommended by Google
 }
