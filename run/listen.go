8f7b8e7ad017419ab28caf8b7df8b061cb4c423d
Amy <amyadzuki@gmail.com>
2018-08-09 14:29:52 +0000

    Update listen.go

--- run/listen.go
+++ run/listen.go
@@ -1,10 +1,17 @@
 package run
 
 import (
+	"os"
+	"syscall"
+
 	"github.com/spf13/cobra"
 
 	"github.com/valyala/fasthttp"
 )
 
 func Listen(cmd *cobra.Command, args []string) {
+	if chroot := vars.Chroot; len(chroot) > 0 {
+		syscall.Chroot(chroot)
+	}
+	os.Chdir(".")
 }
