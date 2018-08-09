28a5befce3350afe4eb544c985ece26421c50d57
Amy <amyadzuki@gmail.com>
2018-08-09 16:42:05 +0000

    Create tls.go

---
+++ vars/tls.go
@@ -0,0 +1,9 @@
+package vars
+
+// Path to TLS certificate file (relative to Chroot, if present)
+var CertPath = "cert.pem"
+
+// Path to TLS key file (relative to Chroot, if present)
+var KeyPath = "key.pem"
+
+
