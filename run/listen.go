5c2cc53b788a2c26349ef9df43a74f855af021a9
Amy <amyadzuki@gmail.com>
2018-08-09 15:19:55 +0000

    Add TLS listener

--- run/listen.go
+++ run/listen.go
@@ -14,7 +14,14 @@ import (
 
 func Listen(cmd *cobra.Command, args []string) {
 	security.Chroot()
-	if err := fasthttp.ListenAndServe(vars.AddrHttp, handlers.Root); err != nil {
-		log.Fatalln("fasthttp.ListenAndServe: \""+err.Error()+"\"")
-	}
+	go func(cmd *cobra.Command, args []string) {
+		if err := fasthttp.ListenAndServe(vars.AddrHttp, handlers.Http); err != nil {
+			log.Fatalln("fasthttp.ListenAndServe: \""+err.Error()+"\"")
+		}
+	}(cmd, args)
+	go func(cmd *cobra.Command, args []string) {
+		if err := fasthttp.ListenAndServeTLS(vars.AddrHttps, handlers.Https); err != nil {
+			log.Fatalln("fasthttp.ListenAndServeTLS: \""+err.Error()+"\"")
+		}
+	}(cmd, args)
 }
