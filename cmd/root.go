1130fdd3eec0128d4e0c357c7fbc976432874472
Amy <amyadzuki@gmail.com>
2018-08-11 11:32:44 +0000

    Update root.go

--- cmd/root.go
+++ cmd/root.go
@@ -12,30 +12,6 @@ import (
 	"github.com/spf13/viper"
 )
 
-var (
-	// Set this in your `init()` function somewhere
-	OverrideRoot func(*cobra.Command, []string)
-)
-
-var (
-	cfgFile string
-)
-
-// rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
-	Use:   ApplicationName,
-	Short: "An application server over HTTP and HTTPS",
-	Long: `An application server over HTTP and HTTPS based on [srv911](https://github.com/amy911/srv911)`,
-	Run: func(cmd *cobra.Command, args []string) {
-		switch {
-		case OverrideRoot == nil:
-			run.Listen(cmd, args)
-		default:
-			OverrideRoot(cmd, args)
-		}
-	},
-}
-
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
@@ -46,30 +22,25 @@ func Execute() {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
+	cobra.OnInitialize(func() {
+		if snek.SysPath == nil {
+			snek.SysPath = syspath.New("amy911", "srv911")
+			log.Print("Initialize github.com/amy911/snek911/snek.SysPath in an `init()` function somewhere!")
+		}
+		if len(cfgFile) > 0 {
+			// Use config file from the flag.
+			viper.SetConfigFile(cfgFile)
+		} else {
+			// Search config in dirs.Config() directory with name "config" (without extension).
+			viper.AddConfigPath(SysPath.Config())
+			viper.SetConfigName("config")
+		}
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file")
-}
+		viper.AutomaticEnv() // read in environment variables that match
 
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if snek.SysPath == nil {
-		snek.SysPath = syspath.New("amy911", "srv911")
-		log.Print("Initialize github.com/amy911/snek911/snek.SysPath in an `init()` function somewhere!")
-	}
-	if len(cfgFile) > 0 {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Search config in dirs.Config() directory with name "ApplicationName" (without extension).
-		viper.AddConfigPath(SysPath.Config())
-		viper.SetConfigName(ApplicationName)
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
+		// If a config file is found, read it in.
+		if err := viper.ReadInConfig(); err == nil {
+			fmt.Println("Using config file:", viper.ConfigFileUsed())
+		}
+	})
 }
