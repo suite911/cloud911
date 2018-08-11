bac58bfd5e3bb140bfb616033f7d4a2c0a7cbf1a
Amy <amyadzuki@gmail.com>
2018-08-11 11:45:00 +0000

    Update main.go

--- main.go
+++ main.go
@@ -3,35 +3,10 @@ package main
 import (
 	"github.com/amy911/snek911/snek"
 
-	"github.com/amy911/srv911/run"
-	"github.com/amy911/srv911/vars"
-
-	"github.com/spf13/cobra"
-)
-
-var (
-	// Set this in your `init()` function somewhere
-	OverrideRoot func(*cobra.Command, []string)
+	"github.com/amy911/srv911/easy"
 )
 
 func main() {
-	snek.InitRoot = func(cmd *cobra.Command) {
-		cmd.PersistentFlags().StringVar(&vars.AddrHttp, "http", "", "Address on which to listen to incoming HTTP traffic")
-		cmd.PersistentFlags().StringVar(&vars.AddrHttps, "https", "", "Address on which to listen to incoming HTTPS traffic")
-		cmd.PersistentFlags().StringVar(&vars.Chroot, "chroot", "", "Path to which to chroot(2)")
-		cmd.PersistentFlags().StringVar(&vars.CertPath, "cert", "", "Path of TLS certificate file")
-		cmd.PersistentFlags().StringVar(&vars.KeyPath, "key", "", "Path of TLS key file")
-		snek.Bind("http", "https", "chroot", "cert", "key")
-		cmd.Short = "An application server over HTTP and HTTPS"
-		cmd.Long = `An application server over HTTP and HTTPS based on [srv911](https://github.com/amy911/srv911)`
-		cmd.Run = func(cmd *cobra.Command, args []string) {
-			switch {
-			case OverrideRoot == nil:
-				run.Listen(cmd, args)
-			default:
-				OverrideRoot(cmd, args)
-			}
-		}
-	}
+	easy.EasyInit()
 	snek.Main()
 }
