299bf77d3369be52bce3077baaee3daf6993af86
Amy <amyadzuki@gmail.com>
2018-08-11 12:52:00 +0000

    Create template.go

---
+++ pages/template.go
@@ -0,0 +1,50 @@
+package pages
+
+import (
+	"errors"
+	"text/template"
+
+	"github.com/amy911/amy911/onfail"
+
+	"github.com/valyala/fasthttp"
+)
+
+var Pages map[string]Page
+
+type Page struct {
+	Author, Autorun, Body, Description, GoogleWebFonts, Head, Keywords, Title string
+
+	Raw []byte
+
+	shell string
+}
+
+func (page *Page) Serve(ctx *fasthttp.RequestCtx, shell string, onFail ...onfail.OnFail) error {
+	fail := func(err error) error {
+		page.shell = shell
+		ctx.Error(500, "Internal Server Error")
+		return onfail.Fail(err, page, onfail.Print, onFail)
+	}
+	if len(page.Raw) > 0 {
+		n, err := ctx.Write(page.Raw)
+		if err == nil {
+			if n == len(raw) {
+				return nil
+			}
+			err = errors.New("Failed to serve complete page")
+		}
+		return fail(err)
+	}
+	tmpl, err := template.New("test").Parse(shell)
+	if err != nil {
+		return fail(err)
+	}
+	if err = tmpl.Execute(ctx, nil); err != nil {
+		return fail(err)
+	}
+	return nil
+}
+
+func (page *Page) Shell() string {
+	return page.shell
+}
