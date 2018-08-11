a21cb48c51ebed91ba2498015afe93906e2d8c96
Amy <amyadzuki@gmail.com>
2018-08-11 12:55:36 +0000

    Update main.go

--- examples/0-hello/main.go
+++ examples/0-hello/main.go
@@ -4,9 +4,14 @@ import (
 	"github.com/amy911/snek911/snek"
 
 	"github.com/amy911/srv911/easy"
+	"github.com/amy911/srv911/pages"
 )
 
 func main() {
 	easy.EasyInit()
+	pages.Pages["index.html"] = pages.Page{
+		Title: "Hello",
+		Body: `Hello, world`,
+	}
 	snek.Main()
 }
