09ddc68709f08c84ec6fd284237589475b0e4558
Amy <amyadzuki@gmail.com>
2018-08-16 15:40:41 +0000

    Create main.go

---
+++ examples/1-basic/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/suite911/cloud911"
+	"github.com/suite911/cloud911/pages"
+
+	"github.com/suite911/env911"
+	"github.com/suite911/env911/app"
+	"github.com/suite911/env911/config"
+)
+
+func init() {
+	env911.InitAll("MYAPP_", nil, "MyCompany", "myapp")
+}
+
+var verbose = false
+
+func main() {
+	flagSet := config.FlagSet()
+	flagSet.BoolVarP(&verbose, "verbose", "v", false, "Use verbose mode")
+
+	pages.Pages["index.html"] = pages.Page{
+		Title: "Hello",
+		Body: `Hello, world`,
+	}
+
+	cloud911.Main(exampleCallback)
+}
+
+func exampleCallback() error {
+	if verbose {
+		fmt.Printfn("Vendor:", app.Vendor()) // prints "MyCompany"
+		fmt.Printfn("App:   ", app.Name()) // prints "myapp"
+		fmt.Printfn("Path:  ", app.Path()) // prints "MyCompany/myapp" on POSIX systems
+	}
+}
