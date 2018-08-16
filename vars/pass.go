6bb334fd3e75c3aa1dc5a7f42217f44d8c67d5a6
Amy <amyadzuki@gmail.com>
2018-08-16 16:01:46 +0000

    Update pass.go

--- vars/pass.go
+++ vars/pass.go
@@ -6,7 +6,8 @@ var Pass = PassType{
 }
 
 type PassType struct {
-	Cert  []byte `json:"cert"`  // TLS certificate and key file data
-	HTTP  string `json:"http"`  // Address on which to listen to HTTP traffic
-	HTTPS string `json:"https"` // Address on which to listen to HTTPS traffic
+	HTTP        string `json:"http"`  // Address on which to listen to HTTP traffic
+	HTTPS       string `json:"https"` // Address on which to listen to HTTPS traffic
+	TLSCertData []byte `json:"cert"`  // TLS certificate and key file data
+	TLSKeyData  []byte `json:"key"`   // TLS certificate and key file data
 }
