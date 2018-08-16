2046ba23f30f0f5a57aee0239a5dc523f5468b0e
Amy <amyadzuki@gmail.com>
2018-08-16 16:17:29 +0000

    Update shell.go

--- pages/shell.go
+++ pages/shell.go
@@ -6,26 +6,31 @@ var Shell = `<!DOCTYPE html>
 <!--[if IE 8]><html class="preIE9"><![endif]-->
 <!--[if gte IE 9]><!--><html><!--<![endif]-->
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<meta name="viewport" content="width=device-width,initial-scale=1">
-{{if .Title}}	<title>{{.Title}}</title>
-{{end}}{{if .Author}}	<meta name="author" content="{{.Author}}">
-{{end}}{{if .Description}}	<meta name="description" content="{{.Description}}">
-{{end}}{{if .Keywords}}	<meta name="keywords" content="{{.Keywords}}">
-{{end}}	<link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon">
-{{if .GoogleFonts}}	<link rel="stylesheet" href="//fonts.googleapis.com/css?family={{.GoogleFonts}}" type="text/css">
-{{end}}	<style type="text/css">
-	//
-	</style>{{.Head}}
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+{{if .Title}}<title>{{.Title}}</title>
+{{end}}{{if .Author}}<meta name="author" content="{{.Author}}">
+{{end}}{{if .Description}}<meta name="description" content="{{.Description}}">
+{{end}}{{if .Keywords}}<meta name="keywords" content="{{.Keywords}}">
+{{end}}<link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon">
+{{if .GoogleFonts}}<link rel="stylesheet" href="//fonts.googleapis.com/css?family={{.GoogleFonts}}" type="text/css">
+{{end}}<style type="text/css"><!-- /*<![CDATA[*/
+/*]]>*/ --></style>{{.Head}}
 </head>
 <body>{{.Body}}
-{{if .Autorun}}	<script type="text/javascript">
-	function autorun(){{{.Autorun}}}
-	if (window.addEventListener) window.addEventListener("load", autorun, false);
-	else if (window.attachEvent) window.attachEvent("onload", autorun);
-	else window.onload = autorun;
-	</script>
+<script type="text/javascript"><!-- //<![CDATA[
+{{if .OnDOMReady}}function onDOMReady(){{{.OnDOMReady}}
+}
+{{end}}{{if .OnPageLoaded}}function onPageLoaded(){{{.OnPageLoaded}}
+}
+{{end}}{{if .OnDOMReady}}if (document.addEventListener) document.addEventListener("DOMContentLoaded", onDOMReady, false);
+else if (document.attachEvent) document.attachEvent("onreadystatechange", onDOMReady);
+else window.onload = onDOMReady;
+{{end}}{{if .OnPageLoaded}}if (window.addEventListener) window.addEventListener("load", onPageLoaded, false);
+else if (window.attachEvent) window.attachEvent("onload", onPageLoaded);
+else window.onload = onPageLoaded;
+{{end}}//]]> --></script>
 {{end}}</body>
 </html>
 `
