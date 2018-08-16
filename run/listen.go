57c88f35e4e1e0f4c5b524df68c6dcbbed4c1c86
Amy <amyadzuki@gmail.com>
2018-08-16 16:00:38 +0000

    Update listen.go

--- run/listen.go
+++ run/listen.go
@@ -9,18 +9,18 @@ import (
 	"github.com/valyala/fasthttp"
 )
 
-func Listen(cmd *cobra.Command, args []string) {
-	go func(cmd *cobra.Command, args []string) {
+func Listen() {
+	go func() {
 		if err := fasthttp.ListenAndServe(vars.HTTP, handlers.HTTP); err != nil {
 			log.Fatalln("fasthttp.ListenAndServe: \""+err.Error()+"\"")
 		}
-	}(cmd, args)
+	}()
 	if err := fasthttp.ListenAndServeTLSEmbed(
 		vars.HTTPS,
-		certData,
-		keyData,
+		vars.CertData,
+		vars.KeyData,
 		handlers.HTTPS,
 	); err != nil {
-		log.Fatalln("fasthttp.ListenAndServeTLS: \""+err.Error()+"\"")
+		log.Fatalln("fasthttp.ListenAndServeTLSEmbed: \""+err.Error()+"\"")
 	}
 }
