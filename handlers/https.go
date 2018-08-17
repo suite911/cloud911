218a50376fa4ca55f35ddd02f200daa67e1fb810
Amy <amyadzuki@gmail.com>
2018-08-17 04:57:54 +0000

    Update https.go

--- handlers/https.go
+++ handlers/https.go
@@ -27,29 +27,29 @@ func https(ctx *fasthttp.RequestCtx) {
 		API(ctx, tail)
 		return
 	}
-	b, ok := PageBytes[path]
-	if !ok {
-		fmt.Fprintf(ctx, "Hello, world!\n\n")
-
-		fmt.Fprintf(ctx, "Request method is %q\n", ctx.Method())
-		fmt.Fprintf(ctx, "RequestURI is %q\n", ctx.RequestURI())
-		fmt.Fprintf(ctx, "Requested path is %q\n", ctx.Path())
-		fmt.Fprintf(ctx, "Host is %q\n", ctx.Host())
-		fmt.Fprintf(ctx, "Query string is %q\n", ctx.QueryArgs())
-		fmt.Fprintf(ctx, "User-Agent is %q\n", ctx.UserAgent())
-		fmt.Fprintf(ctx, "Connection has been established at %s\n", ctx.ConnTime())
-		fmt.Fprintf(ctx, "Request has been started at %s\n", ctx.Time())
-		fmt.Fprintf(ctx, "Serial request number for the current connection is %d\n", ctx.ConnRequestNum())
-		fmt.Fprintf(ctx, "Your ip is %q\n\n", ctx.RemoteIP())
-
-		fmt.Fprintf(ctx, "Raw request is:\n---CUT---\n%s\n---CUT---", &ctx.Request)
-
-		ctx.SetContentType("text/plain; charset=utf8")
-
-		ctx.Error("Not Found", 404)
+	if c, ok := CompiledPages[path]; ok && c != nil {
+		c.Serve(ctx)
 		return
 	}
+	c.SetStatusCode(404)
+	if c, ok := CompiledPages["404"]; ok && c != nil {
+		c.Serve(ctx)
+		return
+	}
+	ctx.SetContentType("text/plain; charset=utf8")
+	fmt.Fprintf(ctx, "Not Found\n\n")
 
-	ctx.Write(b)
+	fmt.Fprintf(ctx, "Request method is %q\n", ctx.Method())
+	fmt.Fprintf(ctx, "RequestURI is %q\n", ctx.RequestURI())
+	fmt.Fprintf(ctx, "Requested path is %q\n", ctx.Path())
+	fmt.Fprintf(ctx, "Host is %q\n", ctx.Host())
+	fmt.Fprintf(ctx, "Query string is %q\n", ctx.QueryArgs())
+	fmt.Fprintf(ctx, "User-Agent is %q\n", ctx.UserAgent())
+	fmt.Fprintf(ctx, "Connection has been established at %s\n", ctx.ConnTime())
+	fmt.Fprintf(ctx, "Request has been started at %s\n", ctx.Time())
+	fmt.Fprintf(ctx, "Serial request number for the current connection is %d\n", ctx.ConnRequestNum())
+	fmt.Fprintf(ctx, "Your ip is %q\n\n", ctx.RemoteIP())
+
+	fmt.Fprintf(ctx, "Raw request is:\n---CUT---\n%s\n---CUT---", &ctx.Request)
 	return
 }
