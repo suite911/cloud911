221fabe151c2d00e586a3737a484bc50cf336edf
Amy <amyadzuki@gmail.com>
2018-08-17 23:07:10 +0000

    Update linux.go

--- droppriv/linux.go
+++ droppriv/linux.go
@@ -2,6 +2,15 @@
 
 package droppriv
 
+import "golang.org/x/sys/unix"
+
+const (
+	SYS_SETUID = unix.SYS_SETUID
+	SYS_SETGID = unix.SYS_SETGID
+	SYS_SETEUID = unix.SYS_SETEUID
+	SYS_SETEGID = unix.SYS_SETEGID
+)
+
 func LinuxDrop() error {
 	return Drop()
 }
