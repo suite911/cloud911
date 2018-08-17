fdac5d99fba7f73bb2a1c1de1c97585dfee93110
Amy <amyadzuki@gmail.com>
2018-08-17 11:37:36 +0000

    Create compiled.go

---
+++ pages/compiled.go
@@ -0,0 +1,38 @@
+package pages
+
+import (
+	"text/template"
+
+	"github.com/suite911/error911/onfail"
+
+	"github.com/valyala/fasthttp"
+)
+
+// CompiledPages is a map of all of the pages after they are compiled.
+var CompiledPages = make(map[string]*CompiledPage)
+
+// Compile compiles each page from Pages and adds it to CompiledPages.
+func Compile(defaultShell *template.Template, onFail ...onfail.OnFail) error {
+	for k, v := range Pages {
+		c, err := v.Compile(defaultShell, onFail...)
+		if err != nil {
+			return err
+		}
+		CompiledPages[k] = c
+	}
+	return nil
+}
+
+// CompiledPage is a type representing a compiled page.
+type CompiledPage struct {
+	ContentType string
+	Bytes       []byte
+}
+
+// Serve serves the CompiledPage over the network.
+func (c *CompiledPage) Serve(ctx *fasthttp.RequestCtx) {
+	if len(c.ContentType) > 0 {
+		ctx.SetContentType(c.ContentType)
+	}
+	ctx.Write(c.Bytes)
+}
