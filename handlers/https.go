942b9299b12f52c17d12ce7eaf507943592a8485
Amy <amyadzuki@gmail.com>
2018-08-17 04:43:16 +0000

    Update https.go

--- handlers/https.go
+++ handlers/https.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"fmt"
 
+	"github.com/suite911/cloud911/pages"
+
 	"github.com/suite911/str911/str"
 
 	"github.com/valyala/fasthttp"
@@ -20,24 +22,34 @@ func HTTPS(ctx *fasthttp.RequestCtx) {
 }
 
 func https(ctx *fasthttp.RequestCtx) {
-	if match, tail := str.CaseHasPrefix(string(ctx.Path()), "/api"); match {
+	path := string(ctx.Path())
+	if match, tail := str.CaseHasPrefix(path, "/api"); match {
 		API(ctx, tail)
 		return
 	}
-	fmt.Fprintf(ctx, "Hello, world!\n\n")
+	b, ok := PageBytes[path]
+	if !ok {
+		fmt.Fprintf(ctx, "Hello, world!\n\n")
 
-	fmt.Fprintf(ctx, "Request method is %q\n", ctx.Method())
-	fmt.Fprintf(ctx, "RequestURI is %q\n", ctx.RequestURI())
-	fmt.Fprintf(ctx, "Requested path is %q\n", ctx.Path())
-	fmt.Fprintf(ctx, "Host is %q\n", ctx.Host())
-	fmt.Fprintf(ctx, "Query string is %q\n", ctx.QueryArgs())
-	fmt.Fprintf(ctx, "User-Agent is %q\n", ctx.UserAgent())
-	fmt.Fprintf(ctx, "Connection has been established at %s\n", ctx.ConnTime())
-	fmt.Fprintf(ctx, "Request has been started at %s\n", ctx.Time())
-	fmt.Fprintf(ctx, "Serial request number for the current connection is %d\n", ctx.ConnRequestNum())
-	fmt.Fprintf(ctx, "Your ip is %q\n\n", ctx.RemoteIP())
+		fmt.Fprintf(ctx, "Request method is %q\n", ctx.Method())
+		fmt.Fprintf(ctx, "RequestURI is %q\n", ctx.RequestURI())
+		fmt.Fprintf(ctx, "Requested path is %q\n", ctx.Path())
+		fmt.Fprintf(ctx, "Host is %q\n", ctx.Host())
+		fmt.Fprintf(ctx, "Query string is %q\n", ctx.QueryArgs())
+		fmt.Fprintf(ctx, "User-Agent is %q\n", ctx.UserAgent())
+		fmt.Fprintf(ctx, "Connection has been established at %s\n", ctx.ConnTime())
+		fmt.Fprintf(ctx, "Request has been started at %s\n", ctx.Time())
+		fmt.Fprintf(ctx, "Serial request number for the current connection is %d\n", ctx.ConnRequestNum())
+		fmt.Fprintf(ctx, "Your ip is %q\n\n", ctx.RemoteIP())
 
-	fmt.Fprintf(ctx, "Raw request is:\n---CUT---\n%s\n---CUT---", &ctx.Request)
+		fmt.Fprintf(ctx, "Raw request is:\n---CUT---\n%s\n---CUT---", &ctx.Request)
 
-	ctx.SetContentType("text/plain; charset=utf8")
+		ctx.SetContentType("text/plain; charset=utf8")
+
+		ctx.Error("Not Found", 404)
+		return
+	}
+
+	ctx.Write(b)
+	return
 }
