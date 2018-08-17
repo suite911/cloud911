f0f0f23e1d7d1dbea7c383eaff8e685bc6f08880
Amy <amyadzuki@gmail.com>
2018-08-17 03:33:44 +0000

    Update and rename pages/shell.go to shells/basic.go

--- shells/basic.go
+++ shells/basic.go
@@ -1,6 +1,6 @@
-package pages
+package shells
 
-var Shell = `<!DOCTYPE html>
+var Basic = `<!DOCTYPE html>
 <!--[if lte IE 6]><html class="preIE7 preIE8 preIE9"><![endif]-->
 <!--[if IE 7]><html class="preIE8 preIE9"><![endif]-->
 <!--[if IE 8]><html class="preIE9"><![endif]-->
