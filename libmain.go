9a6d774d051c2820fd68c36dfc5e89bc93825478
Amy <amyadzuki@gmail.com>
2018-08-17 22:32:46 +0000

    Update libmain.go

--- libmain.go
+++ libmain.go
@@ -1,15 +1,21 @@
 package cloud911
 
 import (
+	"errors"
+	"io/ioutil"
+	"log" // TODO
+	"net"
 	"os"
 
+	"github.com/suite911/cloud911/drop"
 	"github.com/suite911/cloud911/vars"
 
 	"github.com/suite911/env911"
 	"github.com/suite911/env911/config"
 	"github.com/suite911/term911/vt"
 
-	"github.com/mattn/go-isatty"
+	pkgErrors "github.com/pkg/errors"
+	"golang.org/x/sys/unix"
 )
 
 func Main(fns ...func() error) error {
@@ -25,25 +31,79 @@ func Main(fns ...func() error) error {
 	config.LoadAndParse()
 
 	flagSet.SetUsageHeader(
-		"Usage: " + os.Args[0] + " " + vt.SafeU("VERB") + " " + vt.SafeU("OPTIONS") + "\n" +
-		"\n" +
-		"The following are recognized for " + vt.SafeU("VERB") + ":\n" +
-		"  " + vt.SafeB("help")   + "\n    \tPrint this help text and exit.\n" +
-		"  " + vt.SafeB("listen") + "\n    \tListen and serve.\n" +
+		"Usage: " + vt.SafeB("sudo") + " " + os.Args[0] + " " +
+		vt.SafeS("[") + vt.SafeU("OPTIONS") + vt.SafeS("]") + " " +
+		vt.SafeU("UID") + " " + vt.SafeU("GID") + "\n" +
 		"\n" +
 		"The following are recognized for " + vt.SafeU("OPTIONS") + ":\n",
 	)
 
-	if len(os.Args) < 2 {
-		stdin := os.Stdin.Fd()
-		if isatty.IsTerminal(stdin) || isatty.IsCygwinTerminal(stdin) {
-			// Run without args from a terminal: Usage
-			flagSet.Usage()
-			os.Exit(0)
-		}
-		// Child
-		return child(fns)
+	uid, gid := os.Getuid(), os.Getgid()
+	switch {
+	case uid < 0 || gid < 0: // Windows
+		log.Fatalln("Operating system not supported.")
+	case uid > 0 || gid > 0:
+		flagSet.Usage()
+		os.Exit(0)
 	}
-	// Parent
-	return parent(pchroot)
+
+	if err := loadTLSCert(); err != nil {
+		return err
+	}
+	if pchroot == nil {
+		// This can happen if the user's custom FlagSet instance is broken
+		panic("Something is wrong with the custom github.com/suite911/flag911/flag.FlagSet you used with github.com/suite911/env911[/config]")
+	}
+	chroot := *pchroot
+	if len(chroot) > 0 {
+		if err := os.Chdir(chroot); err != nil {
+			return pkgErrors.Wrap(err, "os.Chdir(chroot)")
+		}
+		if err := unix.Chroot(chroot); err != nil {
+			return pkgErrors.Wrap(err, "os.Chroot(chroot)")
+		}
+		if err := os.Chdir("/"); err != nil {
+			return pkgErrors.Wrap(err, "os.Chdir(\"/\")")
+		}
+	}
+	http, err := net.Listen("tcp4", vars.HTTP)
+	if err != nil {
+		return err
+	}
+	https, err := net.Listen("tcp4", vars.HTTPS)
+	if err != nil {
+		return err
+	}
+
+	// Drop privileges
+
+	drop.Drop()
+
+	for _, fn := range fns {
+		if err := fn(); err != nil {
+			return err
+		}
+	}
+	return run.Listen()
+}
+
+func loadTLSCert() error {
+	var err error
+	certPath, keyPath := vars.CertPath, vars.KeyPath
+	if vars.Pass.TLSCertData, err = ioutil.ReadFile(certPath); err != nil {
+		return tlsReadFileError(certPath, keyPath, pkgErrors.Wrap(err, "ioutil.ReadFile(certPath)"))
+	}
+	if vars.Pass.TLSKeyData, err = ioutil.ReadFile(keyPath); err != nil {
+		return tlsReadFileError(certPath, keyPath, pkgErrors.Wrap(err, "ioutil.ReadFile(keyPath)"))
+	}
+	return nil
+}
+
+func tlsReadFileError(certPath, keyPath string, err error) error { // TODO
+	log.Printf(
+		"You need a TLS certificate file and a TLS key file.  "+
+		"By default, these are called \"cert.pem\" and \"key.pem\", respectively.  "+
+		"The paths as configured are %q and %q.", certPath, keyPath)
+	log.Fatalf("ioutil.ReadFile: %q\n", err)
+	return err
 }
