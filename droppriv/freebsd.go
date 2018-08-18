ced9298a886ff47fcc8407eaf27fa788e9518c1e
Amy <amyadzuki@gmail.com>
2018-08-18 14:19:35 +0000

    Update freebsd.go

--- droppriv/freebsd.go
+++ droppriv/freebsd.go
@@ -2,13 +2,11 @@
 
 package droppriv
 
-import "golang.org/x/sys/unix"
-
 const (
-	SYS_SETUID = unix.SYS_SETUID
-	SYS_SETGID = unix.SYS_SETGID
-	SYS_SETEUID = uintptr(183)
-	SYS_SETEGID = uintptr(182)
+	SYS_setuid = uintptr(23)
+	SYS_setgid = uintptr(181)
+	SYS_seteuid = uintptr(183)
+	SYS_setegid = uintptr(182)
 )
 
 func LinuxDrop() error {
