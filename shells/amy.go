7eb5d2639854d65ca4efdebb8486144c73e65741
Amy <amyadzuki@gmail.com>
2018-08-18 16:20:20 +0000

    Update amy.go

--- shells/amy.go
+++ shells/amy.go
@@ -10,10 +10,10 @@ var Amy *template.Template
 
 func init() {
 	text := `<!DOCTYPE html>
-<!--[if lte IE 6}}<html class="preIE7 preIE8 preIE9"><![endif]-->
-<!--[if IE 7}}<html class="preIE8 preIE9"><![endif]-->
-<!--[if IE 8}}<html class="preIE9"><![endif]-->
-<!--[if gte IE 9}}<!--><html><!--<![endif]-->
+<!--[if lte IE 6]><html class="preIE7 preIE8 preIE9"><![endif]-->
+<!--[if IE 7]><html class="preIE8 preIE9"><![endif]-->
+<!--[if IE 8]><html class="preIE9"><![endif]-->
+<!--[if gte IE 9]><!--><html><!--<![endif]-->
 <head>
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
