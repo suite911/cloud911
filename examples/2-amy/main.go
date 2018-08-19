592d653bde9881e2dd58d30c963cd17073f6164f
Amy <amyadzuki@gmail.com>
2018-08-19 01:01:23 +0000

    Update main.go

--- examples/2-amy/main.go
+++ examples/2-amy/main.go
@@ -88,6 +88,7 @@ func main() {
 		FavIcon: favIcon,
 		Footer: string(www["/footer.htm"]),
 		Form: "form",
+		FormAction: "/register/submit",
 		ReCaptchaV2: "6LfgpmoUAAAAAFhnHWF9XHsceqVSFYKH8RDTY-ai",
 		Shell: shells.Amy,
 		Title: "My App - Register",
