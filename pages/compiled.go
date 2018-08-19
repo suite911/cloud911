30c800af159028c55743e4b1d32537a3f4115ccd
Amy <amyadzuki@gmail.com>
2018-08-19 04:38:43 +0000

    Update compiled.go

--- pages/compiled.go
+++ pages/compiled.go
@@ -43,6 +43,7 @@ func (c *CompiledPage) Serve(ctx *fasthttp.RequestCtx) {
 	if proofOfWork := c.ProofOfWork; proofOfWork > 0 {
 		proofOfWork = 1 // TODO: FIXME: DEBUGGING
 		actual := rand.Uint32() & 0xffff
+		actual = 0 // TODO: FIXME: DEBUGGING
 		challenge := strconv.Itoa(int(actual))
 		for j := 0; j < proofOfWork; j++ {
 			b20 := sha1.Sum([]byte(challenge))
