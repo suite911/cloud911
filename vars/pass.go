f2060798503d80ca7b42303719a8630eabf09895
Amy <amyadzuki@gmail.com>
2018-08-20 21:40:04 +0000

    Update pass.go

--- vars/pass.go
+++ vars/pass.go
@@ -6,6 +6,7 @@ var Pass = PassType{
 }
 
 type PassType struct {
+	DataBase    string `json:"db"`    // Path to the main sqlite3 database
 	HTTP        string `json:"http"`  // Address on which to listen to HTTP traffic
 	HTTPS       string `json:"https"` // Address on which to listen to HTTPS traffic
 	TLSCertData []byte `json:"cert"`  // TLS certificate and key file data
