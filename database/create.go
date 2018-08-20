a1f80cad1a765dd1be6e66dad0602df88465d88b
Amy <amyadzuki@gmail.com>
2018-08-20 20:53:43 +0000

    Create create.go

---
+++ database/create.go
@@ -0,0 +1,22 @@
+package database
+
+import (
+	"database/sql"
+
+	"github.com/suite911/query911/query"
+)
+
+const defaultNow = `DEFAULT(CAST(strftime('%s', 'now') AS INTEGER))`
+
+func Create(db *sql.DB) {
+	q := query.Query{DB: db}
+	q.SQL = `
+		CREATE TABLE IF NOT EXISTS "RegisteredUsers" (
+			"id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+			"email" TEXT NOT NULL UNIQUE,
+			"pw" BLOB, -- specifically can be NULL
+			"ts" INTEGER NOT NULL ` + defaultNow + `
+		);
+		CREATE UNIQUE INDEX IF NOT EXISTS "i_RegisteredUsers_email" ON "RegisteredUsers"("email");
+	`
+}
