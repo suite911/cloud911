d0cf54e6060b510d66ff42539c79fb917e3baf15
Amy <amyadzuki@gmail.com>
2018-08-20 23:17:23 +0000

    Update pass.go

--- vars/pass.go
+++ vars/pass.go
@@ -6,11 +6,12 @@ var Pass = PassType{
 }
 
 type PassType struct {
-	DataBase    string `json:"db"`    // Path to the main sqlite3 database
-	HTTP        string `json:"http"`  // Address on which to listen to HTTP traffic
-	HTTPS       string `json:"https"` // Address on which to listen to HTTPS traffic
-	TLSCertData []byte `json:"cert"`  // TLS certificate and key file data
-	TLSKeyData  []byte `json:"key"`   // TLS certificate and key file data
+	CaptchaSecret string `json:"captcha"` // Google reCAPTCHA secret data
+	DataBase      string `json:"db"`      // Path to the main sqlite3 database
+	HTTP          string `json:"http"`    // Address on which to listen to HTTP traffic
+	HTTPS         string `json:"https"`   // Address on which to listen to HTTPS traffic
+	TLSCertData   []byte `json:"cert"`    // TLS certificate and key file data
+	TLSKeyData    []byte `json:"key"`     // TLS certificate and key file data
 
 	FeatureUserProfiles bool `json:"ft_user_profiles"` // Feature toggle for User Profiles
 }
