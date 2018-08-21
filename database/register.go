66757e7c9ada410c3911b397425114b2d8e57a8a
Amy <amyadzuki@gmail.com>
2018-08-21 00:14:45 +0000

    Update register.go

--- database/register.go
+++ database/register.go
@@ -1,5 +1,19 @@
 package database
 
+import (
+	"database/sql"
+
+	"github.com/suite911/cloud911/vars"
+
+	"github.com/suite911/query911/query"
+)
+
 func Register(email string) error {
+	q := query.Query{ DB: DB() }
+	q.SQL = `INSERT OR IGNORE INTO "RegisteredUsers"("email") VALUES(?);`
+	q.Exec(email)
+	if !q.OK() {
+		return q.LastError()
+	}
 	return nil
 }
