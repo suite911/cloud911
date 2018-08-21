d453d2d4744ba9a79aec127ac98b376ba9ea0509
Amy <amyadzuki@gmail.com>
2018-08-21 01:01:14 +0000

    Update register.go

--- database/register.go
+++ database/register.go
@@ -1,13 +1,34 @@
 package database
 
-import "github.com/suite911/query911/query"
+import (
+	"errors"
+	"unicode/utf8"
 
-func Register(email string) error {
+	"github.com/suite911/query911/query"
+
+	"github.com/badoux/checkmail"
+)
+
+func Register(email []byte) (string redir, err error) {
+	if len(email) < 1 {
+		return "?email=missing", errors.New("Empty string")
+	}
+	if !utf8.Valid(email) {
+		return "?email=invalid", errors.New("Malformed UTF-8 string")
+	}
+	email := string(email)
+	if err = checkmail.ValidateFormat(email); err != nil {
+		return "?email=invalid", err
+	}
+	if err = checkmail.ValidateHost(email); err != nil {
+		return "?email=invalid", err
+	}
+
 	q := query.Query{ DB: DB() }
 	q.SQL = `INSERT OR IGNORE INTO "RegisteredUsers"("email") VALUES(?);`
 	q.Exec(email)
 	if !q.OK() {
-		return q.LastError()
+		return redir, q.LastError()
 	}
-	return nil
+	return redir, nil
 }
