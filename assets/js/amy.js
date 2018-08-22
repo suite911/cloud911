199678da156deb7ddbf0467f58525bea60251796
Amy <amyadzuki@gmail.com>
2018-08-22 00:24:16 +0000

    Create amy.js

---
+++ assets/js/amy.js
@@ -0,0 +1,62 @@
+
+function hasClass(elem, className) {
+	return (' ' + elem.className + ' ').indexOf(' ' + className + ' ') > -1;
+}
+
+function replaceState(url) {
+	if(typeof history.replaceState === "function") {
+		history.replaceState(null, null, url);
+		return true
+	}
+	return false
+}
+
+function onDOMReadyHead() {
+	var content = document.getElementById("content");
+	if(content) {
+		content.style.display = "block";
+	}
+	var lightSwitch = document.getElementById("lights");
+	if(lightSwitch) {
+		var lights = cookieGet("lights")
+		switch(lights) {
+		case "off":
+			lightSwitch.checked = false;
+			break;
+		case "on":
+			lightSwitch.checked = true;
+			break;
+		}
+	}
+}
+
+function onDOMReadyTail() {
+	if(location.hash.length >= 2) {
+		var elem = document.getElementById(location.hash.slice(1))
+		if(elem) {
+			if(hasClass(elem, "fragment-block")) {
+				elem.style.display = "block";
+			} else if(hasClass(elem, "fragment-inline")) {
+				elem.style.display = "inline";
+			} else if(hasClass(elem, "fragment-inline-block")) {
+				elem.style.display = "inline-block";
+			}
+			if(!replaceState(location.href.split('#')[0])) {
+				location.hash = '';
+			}
+		}
+	} else if(location.href.slice(-1) == '#') {
+		replaceState(location.href.slice(0, -1))
+	}
+}
+
+function onLightsChanged() {
+	var lightSwitch = document.getElementById("lights");
+	if(lightSwitch) {
+		if(lightSwitch.checked) {
+			cookieSet("lights", "on")
+		} else {
+			cookieSet("lights", "off")
+		}
+	}
+}
