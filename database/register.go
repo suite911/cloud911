1274c57ccc2eb803056f7d8988d50c03a638081e
Amy <amyadzuki@gmail.com>
2018-08-23 17:18:47 +0000

    Update register.go

--- database/register.go
+++ database/register.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"errors"
+	"log"
 	"unicode/utf8"
 
 	"github.com/suite911/query911/query"
@@ -11,10 +12,11 @@ import (
 
 func Register(email string) (redir string, err error) {
 	q := query.Query{ DB: DB() }
-	q.SQL = `INSERT OR IGNORE INTO "RegisteredUsers"("email") VALUES(?);`
+	q.SQL = `INSERT INTO "RegisteredUsers"("email") VALUES(?);`
 	q.Exec(email)
 	if !q.OK() {
-		return redir, q.LastError()
+		log.Printf("INSERT: <%T>: \"%v\"", q.LastError(), q.LastError())
+		return "#already-registered", q.LastError()
 	}
 	return redir, nil
 }
