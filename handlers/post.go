704ae6922f26879b4c138cefb0f7cddf5ea7ae85
Amy <amyadzuki@gmail.com>
2018-08-23 17:14:07 +0000

    Update post.go

--- handlers/post.go
+++ handlers/post.go
@@ -25,8 +25,96 @@ func post(ctx *fasthttp.RequestCtx) {
 	if !utf8.Valid(actionBytes) {
 		return
 	}
+
+	args := ctx.PostArgs()
+	captcha := args.Peek("g-recaptcha-response")
+	var email string
+
+	if emailBytes := args.Peek("email"); len(emailBytes) > 0 {
+		if !utf8.Valid(emailBytes) {
+			ctx.Redirect("#email-invalid", 302)
+			return
+		}
+		email = string(emailBytes)
+		if err = checkmail.ValidateFormat(email); err != nil {
+			ctx.Redirect("#email-invalid", 302)
+			return
+		}
+		if err = checkmail.ValidateHost(email); err != nil {
+			ctx.Redirect("#email-invalid", 302)
+			return
+		}
+	}
+
+	if len(captcha) > 0 {
+		pass, err := VerifyCaptchaSolution(ctx, captcha)
+		if err != nil {
+			ctx.Redirect("#captcha-not-working", 302)
+			return
+		}
+		if !pass {
+			ctx.Redirect("#captcha-failed", 302)
+			return
+		}
+	}
+
+	if childAccount := args.Peek("child-account"); true {
+		log.Printf("child-account: <%T>: \"%v\"", childAccount, childAccount)
+	}
+
 	switch action := string(actionBytes); action {
 	case "register":
-		_/*attempt*/, _/*err*/ = register.Try(ctx)
+		if len(email) < 1 {
+			ctx.Redirect("#email-missing", 302)
+			return
+		}
+		if len(captcha) < 1 {
+			ctx.Redirect("#captcha-missing", 302)
+			return
+		}
+
+		var url string
+		if url, err = database.Register(email); err != nil {
+			if len(url) > 0 {
+				ctx.Redirect(url, 302)
+			}
+			return
+		}
+		if url := vars.Pass.Registered; len(url) > 0 {
+			ctx.Redirect(url, 302)
+			return
+		}
+		ctx.Redirect("#registered", 302)
+		return
 	}
+}
+
+type GoogleCaptchaResponse struct {
+	Success        bool        `json:"success"`
+	ChallengeTS    string      `json:"challenge_ts"`
+	HostName       string      `json:"hostname"`
+	APKPackageName string      `json:"apk_package_name"`
+	ErrorCodes     interface{} `json:"error-codes"`
+}
+
+func VerifyCaptchaSolution(ctx *fasthttp.RequestCtx, solution []byte) (bool, error) {
+	var args fasthttp.Args
+	args.SetBytesV("secret", vars.Pass.CaptchaSecret)
+	args.SetBytesV("response", solution)
+	args.SetBytesV("remoteip", ctx.RequestURI())
+	var statusCode int
+	var body []byte
+	if statusCode, body, err = fasthttp.Post(nil, "https://www.google.com/recaptcha/api/siteverify", &args); err != nil {
+		return false, pkgErrors.Wrap(err, "fasthttp.Post")
+	}
+	if statusCode != 200 {
+		log.Printf("Google replied %d\n--- REPLY BODY ---\n%v\n--- END OF REPLY BODY ---", statusCode, string(body))
+		return false, pkgErrors.Wrap(errors.New(strconv.Itoa(statusCode)), "fasthttp.Post")
+	}
+	var resp GoogleCaptchaResponse
+	if err = json.Unmarshal(body, &resp); err != nil {
+		log.Printf("Google replied %d\n--- REPLY BODY ---\n%v\n--- END OF REPLY BODY ---", statusCode, resp)
+		return false, pkgErrors.Wrap(err, "json.Unmarshal(body, &resp)")
+	}
+	return resp.Success, nil
 }
