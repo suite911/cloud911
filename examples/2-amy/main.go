524ebf6998ad232cfce148f4b8aee287924a252d
Amy <amyadzuki@gmail.com>
2018-08-23 17:45:52 +0000

    Update main.go

--- examples/2-amy/main.go
+++ examples/2-amy/main.go
@@ -91,7 +91,6 @@ func main() {
 		ContentSubTitle: "Create a new account.",
 		CSS: string(www["/register.css"]),
 		Form: "form",
-		FormAction: "/download",
 		NoScript: "Hello, fellow NoScript user!  This is awkward but could you pretty please whitelist " +
 			"my registration page?  You see, the Captcha gods are picky and like to use JavaScript " +
 			"to do ...well whatever Captcha gods do with JavaScript.  So anyway it would just make " +
