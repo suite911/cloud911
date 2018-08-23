9ab6908f9ff7cdffdf789c5374f4d93b9cc1d431
Amy <amyadzuki@gmail.com>
2018-08-23 17:14:13 +0000

    Update register.go

--- register/register.go
+++ register/register.go
@@ -13,63 +13,7 @@ import (
 	"github.com/valyala/fasthttp"
 )
 
-type GoogleCaptchaResponse struct {
-	Success        bool        `json:"success"`
-	ChallengeTS    string      `json:"challenge_ts"`
-	HostName       string      `json:"hostname"`
-	APKPackageName string      `json:"apk_package_name"`
-	ErrorCodes     interface{} `json:"error-codes"`
-}
+func Try(ctx *fasthttp.RequestCtx, email, captcha []byte) (attempt bool, err error) {
 
-func Try(ctx *fasthttp.RequestCtx) (attempt bool, err error) {
-	attempt = ctx.IsPost()
-	if !attempt {
-		return
-	}
-	argsRecv := ctx.PostArgs()
-	email := argsRecv.Peek("email")
-	captcha := argsRecv.Peek("g-recaptcha-response")
-	if len(email) < 1 {
-		ctx.Redirect("#email-missing", 302)
-		return
-	}
-	if len(captcha) < 1 {
-		ctx.Redirect("#captcha-missing", 302)
-		return
-	}
-	var args fasthttp.Args
-	args.SetBytesV("secret", vars.Pass.CaptchaSecret)
-	args.SetBytesV("response", captcha)
-	args.SetBytesV("remoteip", ctx.RequestURI())
-	var statusCode int
-	var body []byte
-	if statusCode, body, err = fasthttp.Post(nil, "https://www.google.com/recaptcha/api/siteverify", &args); err != nil {
-		return
-	}
-	if statusCode != 200 {
-		log.Printf("Google replied %d\n--- REPLY BODY ---\n%v\n--- END OF REPLY BODY ---", statusCode, string(body))
-		return attempt, pkgErrors.Wrap(errors.New(strconv.Itoa(statusCode)), "fasthttp.Post")
-	}
-	var resp GoogleCaptchaResponse
-	if err = json.Unmarshal(body, &resp); err != nil {
-		log.Printf("Google replied %d\n--- REPLY BODY ---\n%v\n--- END OF REPLY BODY ---", statusCode, resp)
-		return attempt, pkgErrors.Wrap(err, "json.Unmarshal(body, &resp)")
-	}
-	if !resp.Success {
-		ctx.Redirect("#captcha-failed", 302)
-		return
-	}
-	var url string
-	if url, err = database.Register(email); err != nil {
-		if len(url) > 0 {
-			ctx.Redirect(url, 302)
-		}
-		return
-	}
-	if url := vars.Pass.Registered; len(url) > 0 {
-		ctx.Redirect(url, 302)
-		return
-	}
-	ctx.Redirect("#registered", 302)
-	return
+
 }
