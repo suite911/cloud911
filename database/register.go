da24a9acefb30f6641f2dfcc7ff6a90076db3e0b
Amy <amyadzuki@gmail.com>
2018-08-23 18:27:12 +0000

    Update register.go

--- database/register.go
+++ database/register.go
@@ -3,16 +3,28 @@ package database
 import (
 	"log"
 
+	"github.com/suite911/cloud911/vars"
+
 	"github.com/suite911/query911/query"
 )
 
-func Register(email string) (redir string, err error) {
+func Register(email string) string {
 	q := query.Query{ DB: DB() }
 	q.SQL = `INSERT INTO "RegisteredUsers"("email") VALUES(?);`
 	q.Exec(email)
 	if !q.OK() {
-		log.Printf("INSERT: <%T>: \"%v\"", q.LastError(), q.LastError())
-		return "#already-registered", q.LastError()
+		err := q.LastError()
+		if str.CaseHasPrefix(err.Error(), "unique") {
+			if url := vars.AlreadyRegistered; len(url) > 0 {
+				return url
+			}
+			return "#already-registered"
+		}
+		log.Println(err)
+		return "#database-error"
 	}
-	return redir, nil
+	if url := vars.Registered; len(url) > 0 {
+		return url
+	}
+	return "#registered"
 }
