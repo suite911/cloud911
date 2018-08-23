09f9c697444dfb938ca2d0e5ccb8a504726810d6
Amy <amyadzuki@gmail.com>
2018-08-23 17:09:37 +0000

    Update register.go

--- database/register.go
+++ database/register.go
@@ -9,24 +9,10 @@ import (
 	"github.com/badoux/checkmail"
 )
 
-func Register(email []byte) (redir string, err error) {
-	if len(email) < 1 {
-		return "?email=missing", errors.New("Empty string")
-	}
-	if !utf8.Valid(email) {
-		return "?email=invalid", errors.New("Malformed UTF-8 string")
-	}
-	stringEmail := string(email)
-	if err = checkmail.ValidateFormat(stringEmail); err != nil {
-		return "?email=invalid", err
-	}
-	if err = checkmail.ValidateHost(stringEmail); err != nil {
-		return "?email=invalid", err
-	}
-
+func Register(email string) (redir string, err error) {
 	q := query.Query{ DB: DB() }
 	q.SQL = `INSERT OR IGNORE INTO "RegisteredUsers"("email") VALUES(?);`
-	q.Exec(stringEmail)
+	q.Exec(email)
 	if !q.OK() {
 		return redir, q.LastError()
 	}
