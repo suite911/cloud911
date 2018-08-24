b8afa4e941819ad3a73dc187f189afb35da1c735
Amy <amyadzuki@gmail.com>
2018-08-24 18:16:36 +0000

    Update register.go

--- database/register.go
+++ database/register.go
@@ -9,11 +9,15 @@ import (
 	"github.com/suite911/str911/str"
 )
 
-func Register(username, email string, captcha float64, minor bool, emwho, emhow, emrel string) string {
+func Register(username, email string, scores []float64, minor bool, emwho, emhow, emrel string) string {
+	if len(scores) != 3 {
+		log.Println("len(scores):", len(scores), ":", scores)
+		return "#something-went-wrong"
+	}
 	q := query.Query{ DB: DB() }
-	q.SQL = `INSERT INTO "RegisteredUsers"("un", "email", "captcha", "minor", "emwho", "emhow", "emrel")`+
-		` VALUES(?, ?, ?, ?, ?, ?, ?);`
-	q.Exec(username, email, captcha, minor, emwho, emhow, emrel)
+	q.SQL = `INSERT INTO "RegisteredUsers"("un", "email", "conload", "conchange", "consubmit", ` +
+		`"minor", "emwho", "emhow", "emrel") VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?);`
+	q.Exec(username, email, scores[0], scores[1], scores[2], minor, emwho, emhow, emrel)
 	if !q.OK() {
 		err := q.LastError()
 		if str.CaseHasPrefix(err.Error(), "unique") {
