115cf496b471201e9dda881bacb23c358f17dde8
Amy <amyadzuki@gmail.com>
2018-08-24 07:05:51 +0000

    Update create.go

--- database/create.go
+++ database/create.go
@@ -20,11 +20,13 @@ func Create() error {
 	q.SQL = `
 		CREATE TABLE IF NOT EXISTS "RegisteredUsers" (
 			"id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-			"email" TEXT NOT NULL UNIQUE,
+			"un" TEXT NOT NULL UNIQUE,
+			"email" TEXT NOT NULL,
 			"pw" BLOB DEFAULT(NULL),
 			"regd" INTEGER NOT NULL ` + defaultNow + `,
 			"verd" INTEGER NOT NULL DEFAULT(0),
-			"bal" INTEGER NOT NULL DEFAULT(0)
+			"bal" INTEGER NOT NULL DEFAULT(0),
+			"captcha" REAL NOT NULL
 		);
 	`
 	q.Exec()
@@ -32,7 +34,14 @@ func Create() error {
 		return q.LastError()
 	}
 	q.SQL = `
-		CREATE UNIQUE INDEX IF NOT EXISTS "idx_RegisteredUsers_email" ON "RegisteredUsers"("email");
+		CREATE UNIQUE INDEX IF NOT EXISTS "idx_RegisteredUsers_un" ON "RegisteredUsers"("un");
+	`
+	q.Exec()
+	if !q.OK() {
+		return q.LastError()
+	}
+	q.SQL = `
+		CREATE INDEX IF NOT EXISTS "idx_RegisteredUsers_email" ON "RegisteredUsers"("email");
 	`
 	q.Exec()
 	if !q.OK() {
