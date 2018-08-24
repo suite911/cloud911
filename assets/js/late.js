65d5d25328338a382876727ab30f3682d58e2df9
Amy <amyadzuki@gmail.com>
2018-08-24 17:21:59 +0000

    Update late.js

--- assets/js/late.js
+++ assets/js/late.js
@@ -1,20 +1,27 @@
 'use strict';
 
 function amy_onChangedFull(event) {
+	console.log("function amy_onChangedFull(event) {");
 	var captcha = document.getElementById("captcha-onchange");
 	if(captcha) {
+		console.log("  if(captcha)");
 		grecaptcha.ready(function() {
+			console.log("    amy_onChangedFull->grecaptcha.ready->anon");
 			grecaptcha.execute(amy_reCAPTCHAv3SiteKey, {action: "change"}).then(function(token) {
+				console.log("      amy_onChangedFull->grecaptcha.ready->anon->grecaptcha.execute.then");
 				captcha.value = token;
 				amy_onChangedFull2();
 			});
 		});
 	} else {
+		console.log("  if(!captcha)");
 		amy_onChangedFull2();
 	}
+	console.log("} // amy_onChangedFull");
 }
 
 function amy_onChangedFull2() {
+	console.log("  function amy_onChangedFull2() {");
 	amy_onChangedHead();
 	if(typeof amy_onChanged === "function") {
 		var ok = amy_onChanged();
@@ -23,6 +30,7 @@ function amy_onChangedFull2() {
 		}
 	}
 	amy_onChangedTail();
+	console.log("  } // amy_onChangedFull2");
 }
 
 function amy_onChangedHead() {
@@ -111,20 +119,27 @@ function amy_onLightsChanged(event) {
 }
 
 function amy_onPageLoadedFull() {
+	console.log("function amy_onPageLoadedFull(event) {");
 	var captcha = document.getElementById("captcha-onload");
 	if(captcha) {
+		console.log("  if(captcha)");
 		grecaptcha.ready(function() {
+			console.log("    amy_onPageLoadedFull->grecaptcha.ready->anon");
 			grecaptcha.execute(amy_reCAPTCHAv3SiteKey, {action: "load"}).then(function(token) {
+				console.log("      amy_onPageLoadedFull->grecaptcha.ready->anon->grecaptcha.execute.then");
 				captcha.value = token;
 				amy_onPageLoadedFull2();
 			});
 		});
 	} else {
+		console.log("  if(!captcha)");
 		amy_onPageLoadedFull2();
 	}
+	console.log("} // amy_onPageLoadedFull");
 }
 
 function amy_onPageLoadedFull2() {
+	console.log("  function amy_onPageLoadedFull2() {");
 	amy_onPageLoadedHead();
 	if(typeof amy_onPageLoaded === "function") {
 		var ok = amy_onPageLoaded();
@@ -133,6 +148,7 @@ function amy_onPageLoadedFull2() {
 		}
 	}
 	amy_onPageLoadedTail();
+	console.log("  } // amy_onPageLoadedFull2");
 }
 
 function amy_onPageLoadedHead() {
@@ -145,20 +161,27 @@ function amy_onPageLoadedTail() {
 }
 
 function amy_onSubmitFull() {
+	console.log("function amy_onSubmitFull(event) {");
 	var captcha = document.getElementById("captcha-onsubmit");
 	if(captcha) {
+		console.log("  if(captcha)");
 		grecaptcha.ready(function() {
+			console.log("    amy_onSubmitFull->grecaptcha.ready->anon");
 			grecaptcha.execute(amy_reCAPTCHAv3SiteKey, {action: "submit"}).then(function(token) {
+				console.log("      amy_onSubmitFull->grecaptcha.ready->anon->grecaptcha.execute.then");
 				captcha.value = token;
 				amy_onSubmitFull2();
 			});
 		});
 	} else {
+		console.log("  if(!captcha)");
 		amy_onSubmitFull2();
 	}
+	console.log("} // amy_onSubmitFull");
 }
 
 function amy_onSubmitFull2() {
+	console.log("  function amy_onSubmitFull2() {");
 	var lightSwitch = document.getElementById("lights-off");
 	if(lightSwitch) {
 		var lightAnalytics = document.getElementById("analytics-lights");
@@ -183,6 +206,7 @@ function amy_onSubmitFull2() {
 	if(amy_onSubmitted === "function") {
 		return amy_onSubmitted();
 	}
+	console.log("  } // amy_onSubmitFull2");
 }
 
 if (typeof document.addEventListener === "function") {
