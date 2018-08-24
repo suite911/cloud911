e16c0ed8094afbd465b35c471902d42f5042748c
Amy <amyadzuki@gmail.com>
2018-08-24 03:03:30 +0000

    Update amy.go

--- shells/amy.go
+++ shells/amy.go
@@ -25,185 +25,24 @@ func init() {
 {{end}}{{if .FavIcon}}<link rel="shortcut icon" href="{{.FavIcon}}" type="image/vnd.microsoft.icon" />
 {{end}}{{if .GoogleFonts}}<link rel="stylesheet" href="//fonts.googleapis.com/css?family={{.GoogleFonts}}" type="text/css" />
 {{end}}<link rel="stylesheet" href="//rawgit.com/suite911/cloud911/master/assets/css/amy.css" type="text/css" />
+<link rel="stylesheet" href="/1.css" type="text/css" />
+{{if .InlineCSS}}
 <style type="text/css"><!-- /*<![CDATA[*/
-{{if .CSSHead}}{{.CSSHead}}
-
-{{end}}body, .sans {
-	font-family: {{if .Sans}}"{{.Sans}}", {{end}}sans-serif;
-}
-
-code, .mono {
-	font-family: {{if .Mono}}"{{.Mono}}", {{end}}monospace;
-}
-
-a {
-	color: {{.Vars.LinkFg}};
-}
-
-a:hover {
-	color: {{.Vars.LinkHover}};
-}
-
-.page-outer {
-	background-color: {{.Vars.Light_Bg}};
-	color: {{.Vars.Light_Fg}};
-}
-
-.page-inner {
-	padding: 0 0 {{.Vars.FooterHeight}} 0;
-}
-
-header {
-	background-color: {{.Vars.Light_Header_Bg}};
-	color: {{.Vars.Light_Header_Fg}};
-}
-
-footer {
-	background-color: {{.Vars.Light_Footer_Bg}};
-	color: {{.Vars.Light_Footer_Fg}};
-	font-size: {{.Vars.FooterHeight}};
-	height: {{.Vars.FooterHeight}};
-	line-height: {{.Vars.FooterHeight}};
-	margin: -{{.Vars.FooterHeight}} 0 0 0;
-}
-
-footer a, footer a:active, footer a:focus, footer a:link, footer a:visited {
-	color: {{.Vars.Light_Footer_Fg}};
-}
-
-footer a:hover {
-	color: {{.Vars.Light_Footer_Fg}};
-}
-
-input[type=checkbox].lights-off:checked + div header {
-	background-color: {{.Vars.Dark_Header_Bg}};
-	color: {{.Vars.Dark_Header_Fg}};
-}
-
-input[type=checkbox].lights-off:checked ~ div {
-	background-color: {{.Vars.Dark_Bg}};
-	color: {{.Vars.Dark_Fg}};
-}
-
-input[type=checkbox].lights-off:checked ~ footer {
-	background-color: {{.Vars.Dark_Footer_Bg}};
-	color: {{.Vars.Dark_Footer_Fg}};
-}
-
-input[type=checkbox].lights-off:checked ~ footer a,
-input[type=checkbox].lights-off:checked ~ footer a:active,
-input[type=checkbox].lights-off:checked ~ footer a:focus,
-input[type=checkbox].lights-off:checked ~ footer a:link,
-input[type=checkbox].lights-off:checked ~ footer a:visited {
-	color: {{.Vars.Dark_Footer_Fg}};
-}
-
-input[type=checkbox].lights-off:checked ~ footer a:hover {
-	color: {{.Vars.Dark_Footer_Fg}};
-}
-
-/* Inner elements */
-
-div.topnav {
-	font-size: {{.Vars.TopNavHeight}};
-	height: calc({{.Vars.TopNavHeight}} + 20px);
-	line-height: {{.Vars.TopNavHeight}};
-}
-
-span.topnav {
-	background-color: {{.Vars.Light_TopNav_Bg}};
-	color: {{.Vars.Light_TopNav_Fg}};
-}
-
-span.topnav:hover {
-	background-color: {{.Vars.Light_TopNav_Bg_Hover}};
-	color: {{.Vars.Light_TopNav_Fg_Hover}};
-}
-
-input[type=checkbox].lights-off:checked + div header span.topnav {
-	background-color: {{.Vars.Dark_TopNav_Bg}};
-	color: {{.Vars.Dark_TopNav_Fg}};
-}
-
-input[type=checkbox].lights-off:checked + div header span.topnav:hover {
-	background-color: {{.Vars.Dark_TopNav_Bg_Hover}};
-	color: {{.Vars.Dark_TopNav_Fg_Hover}};
-}
-
-/* The "Lights Off" toggle */
-label.lights-off {
-	background-color: {{.Vars.Light_Night_Bg}};
-	border: 2px solid {{.Vars.Light_Night_Border}};
-	color: {{.Vars.Light_Night_Fg}};
-}
-
-label.lights-off:hover {
-	background-color: {{.Vars.Light_Night_Bg_Hover}};
-	border-color: {{.Vars.Light_Night_Border_Hover}};
-	color: {{.Vars.Light_Night_Fg_Hover}};
-}
-
-input[type=checkbox].lights-off:checked + div header label.lights-off {
-	background-color: {{.Vars.Dark_Night_Bg}};
-	border-color: {{.Vars.Dark_Night_Border}};
-	color: {{.Vars.Dark_Night_Fg}};
-}
-
-input[type=checkbox].lights-off:checked + div header label.lights-off:hover {
-	background-color: {{.Vars.Dark_Night_Bg_Hover}};
-	border-color: {{.Vars.Dark_Night_Border_Hover}};
-	color: {{.Vars.Dark_Night_Fg_Hover}};
-}
-
-input[type=text], input[type=password] {
-	background-color: {{.Vars.EntryBg}};
-	border: 2px solid {{.Vars.EntryBorder}};
-	color: {{.Vars.EntryFg}};
-	padding: {{.Vars.PaddingWidgetVert}} {{.Vars.PaddingWidgetHorz}};
-}
-
-input[type=text]:focus, input[type=password]:focus {
-	background-color: {{.Vars.FocusBg}};
-	border: 2px solid {{.Vars.FocusBorder}};
-	color: {{.Vars.FocusFg}};
-}
-
-input[type=submit] {
-	background-color: {{.Vars.ButtonSubmitBg}};
-	color: {{.Vars.ButtonSubmitFg}};
-	padding: {{.Vars.PaddingWidgetVert}} {{.Vars.PaddingWidgetHorz}};
-}
-
-input[type=submit]:hover {
-	background-color: {{.Vars.ButtonSubmitHover}};
-}
-
-input[type=submit]:disabled {
-	background-color: {{.Vars.ButtonDisabledBg}};
-	color: {{.Vars.ButtonDisabledFg}};
-}
-
-div.copyright {
-	font-size: {{.Vars.FooterHeight}};
-	height: {{.Vars.FooterHeight}};
-	line-height: {{.Vars.FooterHeight}};
-	margin: -{{.Vars.FooterHeight}} 0 0 0;
-}
-{{if .CSS}}
-{{.CSS}}
-{{end}}{{if .CSSTail}}
-{{.CSSTail}}
-{{end}}/*]]>*/ --></style>{{if .Head}}
-{{.Head}}{{end}}
-{{if .Form}}{{if .ReCaptchaV2}}<script src='//www.google.com/recaptcha/api.js' async defer></script>
-{{end}}<script type="text/javascript"><!-- //<![CDATA[
-	function onSubmit(token) {
-{{.OnWillSubmit}}
-		document.getElementById("{{.Form}}").submit();
-{{.OnSubmitted}}
-	}
+{{.InlineCSS}}
+/*]]>*/ --></style>
+{{end}}
+{{if .Head}}
+{{.Head}}
+{{end}}
+<script type="text/javascript" src="//rawgit.com/suite911/cloud911/master/assets/js/early.js"></script>
+<script type="text/javascript" src='/1.js'></script>
+{{if .ReCaptchaV2}}<script src='//www.google.com/recaptcha/api.js' async defer></script>{{end}}
+{{if .InlineJavaScript}}
+<script type="text/javascript"><!-- //<![CDATA[
+{{.InlineJavaScript}}
 //]]> --></script>
-{{end}}</head>
+{{end}}
+</head>
 <body>{{.BodyHead}}{{if .Body}}{{.Body}}{{else}}
 <input type="checkbox" class="lights-off" id="lights-off" checked />
 <div class="page-outer"><div class="page-inner">
@@ -225,42 +64,36 @@ div.copyright {
 		<p>{{.ContentSubTitle}}</p>{{end}}
 		<hr />
 	</div>{{end}}
-	<div id="content" class="content"{{if .NoScript}} style="display:none"{{end}}>{{.ContentHead}}{{if .Form}}
-		<div class="form"><form id="{{.Form}}" action="{{.FormAction}}" method="POST">{{end}}{{.Content}}{{if .Form}}
-		{{if .ReCaptchaV2}}<input type="hidden" id="recaptcha-token" name="recaptcha-token" value="" />
-		<input type="submit" id="bsubmit" class="g-recaptcha card" data-sitekey="{{.ReCaptchaV2}}"
-			value="Submit" data-callback='onSubmit' />
-		{{end}}<br /></form></div>{{end}}{{.ContentTail}}
-	</div>{{if .NoScript}}
-	<noscript>{{.NoScript}}</noscript>{{end}}
+	<div id="content"{{if .NoScript}} class="display-none"{{end}}>
+{{.ContentHead}}
+{{if .Form}}
+		<div class="form">
+			<form id="form" action="{{.FormAction}}" method="POST">
+{{end}}
+{{.Content}}
+{{if .Form}}
+{{if .ReCaptchaV2}}
+				<input type="hidden" id="recaptcha-token" name="recaptcha-token" value="" />
+				<input type="submit" id="bsubmit"
+					class="g-recaptcha card"
+					data-callback='onSubmit'
+					data-sitekey="{{.ReCaptchaV2}}"
+					value="Submit"
+					/>
+{{end}}
+			<br />
+			</form>
+		</div>
+{{end}}
+{{.ContentTail}}
+	</div>
+{{if .NoScript}}<noscript>{{.NoScript}}</noscript>{{end}}
 </div></div>
 <footer class="footer">{{.FooterHead}}{{.Footer}}{{.FooterTail}}
 	<div class="copyright">{{.Copyright}}</div>
 </footer>
 {{end}}{{.BodyTail}}
-<script type="text/javascript" src="//rawgit.com/suite911/cloud911/master/assets/js/amy.js"></script>
-<script type="text/javascript"><!-- //<![CDATA[
-{{if .JavaScriptHead}}{{.JavaScriptHead}}
-{{end}}{{if .JavaScript}}
-{{.JavaScript}}
-{{end}}
-function onDOMReady(){
-	amy_onDOMReadyHead()
-{{.OnDOMReady}}
-	amy_onDOMReadyTail()
-}
-function onPageLoaded(){
-{{.OnPageLoaded}}
-	amy_cookieAgree();
-}
-if (document.addEventListener) document.addEventListener("DOMContentLoaded", onDOMReady, false);
-else if (document.attachEvent) document.attachEvent("onreadystatechange", onDOMReady);
-else window.onload = onDOMReady;
-if (window.addEventListener) window.addEventListener("load", onPageLoaded, false);
-else if (window.attachEvent) window.attachEvent("onload", onPageLoaded);
-else window.onload = onPageLoaded;
-{{if .JavaScriptTail}}{{.JavaScriptTail}}
-{{end}}//]]> --></script>
+<script type="text/javascript" src="//rawgit.com/suite911/cloud911/master/assets/js/late.js"></script>
 </body>
 </html>
 `
