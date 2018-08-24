3cbafcd50612470fa6bedb46ad31e3a021250492
Amy <amyadzuki@gmail.com>
2018-08-24 03:10:45 +0000

    Update late.js

--- assets/js/late.js
+++ assets/js/late.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function amy_onDOMReadyFull() {
 	amy_onDOMReadyHead();
 	if(typeof amy_onDOMReady === "function") {
@@ -7,6 +9,61 @@ function amy_onDOMReadyFull() {
 		}
 	}
 	amy_onDOMReadyTail();
+}
+
+function amy_onDOMReadyHead() {
+	var content = document.getElementById("content");
+	if(content) {
+		content.style.display = "block";
+	}
+	var lightSwitch = document.getElementById("lights-off");
+	if(lightSwitch) {
+		var lights = amy_getCookie("lights");
+		switch(lights) {
+		case "off":
+			lightSwitch.checked = true;
+			break;
+		case "on":
+			lightSwitch.checked = false;
+			break;
+		default:
+			lightSwitch.checked = false;
+			amy_setCookie("lights", "on");
+			break;
+		}
+		amy_addEventListener(lightSwitch, "change", amy_onLightsChanged);
+	}
+}
+
+function amy_onDOMReadyTail() {
+	if(location.hash.length >= 2) {
+		var elem = document.getElementById(location.hash.slice(1))
+		if(elem) {
+			if(amy_hasClass(elem, "fragment-block")) {
+				elem.style.display = "block";
+			} else if(amy_hasClass(elem, "fragment-inline")) {
+				elem.style.display = "inline";
+			} else if(amy_hasClass(elem, "fragment-inline-block")) {
+				elem.style.display = "inline-block";
+			}
+			if(!replaceState(location.href.split('#')[0])) {
+				location.hash = '';
+			}
+		}
+	} else if(location.href.slice(-1) == '#') {
+		replaceState(location.href.slice(0, -1));
+	}
+}
+
+function amy_onLightsChanged(event) {
+	var lightSwitch = document.getElementById("lights-off");
+	if(lightSwitch) {
+		if(lightSwitch.checked) {
+			amy_setCookie("lights", "off");
+		} else {
+			amy_setCookie("lights", "on");
+		}
+	}
 }
 
 function amy_onPageLoadedFull() {
