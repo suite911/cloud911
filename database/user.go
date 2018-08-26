8476a72685c68eda35dea9217a5807ac1679d671
Amy <amyadzuki@gmail.com>
2018-08-26 07:08:09 +0000

    Update user.go

--- database/user.go
+++ database/user.go
@@ -9,6 +9,30 @@ import (
 	"github.com/suite911/str911/str"
 )
 
-func User(email, username string) int64 {
-	q := query.Query{}
+type QueryUser struct {
+	rowID int64 `json:"rowid"`
+	id    int64 `json:"id"`
+	regd  int64 `json:"regd"`
+	verd  int64 `json:"verd"`
+	minor bool  `json:"minor"`
+}
+
+func User(email, username string) (*QueryUser, error) {
+	q := query.Query{DB: DB()}
+	q.SQL = `SELECT "_ROWID_", "id", "regd", "verd", "minor" FROM "RegisteredUsers" WHERE "email" = ? AND "un" = ?;`
+	q.Query(email, username)
+	if !q.OK() {
+		return nil, q.LastError()
+	}
+	if !q.NextOrClose() {
+		return nil, q.LastError() // probably nil, which is what we want: it means no result
+	}
+	resp := new(QueryUser)
+	var minor int64
+	q.ScanClose(&resp.rowID, &resp.id, &resp.regd, &resp.verd, &minor)
+	resp.minor = minor != 0
+	if !q.OK() {
+		return nil, q.LastError()
+	}
+	return resp, nil
 }
