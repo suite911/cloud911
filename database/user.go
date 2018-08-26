4da8a9d722d5f49c4976b4199629bce4c75ed177
Amy <amyadzuki@gmail.com>
2018-08-26 06:57:05 +0000

    Create user.go

---
+++ database/user.go
@@ -0,0 +1,14 @@
+package database
+
+import (
+	"log"
+
+	"github.com/suite911/cloud911/vars"
+
+	"github.com/suite911/query911/query"
+	"github.com/suite911/str911/str"
+)
+
+func User(email, username string) int64 {
+	q := query.Query{}
+}
