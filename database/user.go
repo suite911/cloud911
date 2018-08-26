c3396a9ec3a7342e099c6889c72caa31c4ebdc0b
Amy <amyadzuki@gmail.com>
2018-08-26 22:02:45 +0000

    Update user.go

--- database/user.go
+++ database/user.go
@@ -7,12 +7,11 @@ type QueryUser struct {
 	id    int64 `json:"id"`
 	regd  int64 `json:"regd"`
 	verd  int64 `json:"verd"`
-	minor bool  `json:"minor"`
 }
 
 func User(email, username string) (*QueryUser, error) {
 	q := query.Query{DB: DB()}
-	q.SQL = `SELECT "_ROWID_", "id", "regd", "verd", "minor" FROM "RegisteredUsers" WHERE "email" = ? AND "un" = ?;`
+	q.SQL = `SELECT "_ROWID_", "id", "regd", "verd" FROM "RegisteredUsers" WHERE "email" = ? AND "un" = ?;`
 	q.Query(email, username)
 	if err := q.ErrorLogNow(); err != nil {
 		return nil, err
@@ -21,9 +20,7 @@ func User(email, username string) (*QueryUser, error) {
 		return nil, q.ErrorLogNow() // probably nil, which is what we want: it means no result
 	}
 	resp := new(QueryUser)
-	var minor int64
-	q.ScanClose(&resp.rowID, &resp.id, &resp.regd, &resp.verd, &minor)
-	resp.minor = minor != 0
+	q.ScanClose(&resp.rowID, &resp.id, &resp.regd, &resp.verd)
 	if err := q.ErrorLogNow(); err != nil {
 		return nil, err
 	}
