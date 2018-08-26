5fb6c12baf7fc181e080e399bb288f137440e79f
Amy <amyadzuki@gmail.com>
2018-08-26 22:00:12 +0000

    Update user.go

--- database/user.go
+++ database/user.go
@@ -1,13 +1,6 @@
 package database
 
-import (
-	"log"
-
-	"github.com/suite911/cloud911/vars"
-
-	"github.com/suite911/query911/query"
-	"github.com/suite911/str911/str"
-)
+import "github.com/suite911/query911/query"
 
 type QueryUser struct {
 	rowID int64 `json:"rowid"`
