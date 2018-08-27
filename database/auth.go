42e09adad4b0222e0f3f7ac2d168c0d7492fd9b2
Amy <amyadzuki@gmail.com>
2018-08-27 00:04:38 +0000

    Update auth.go

--- database/auth.go
+++ database/auth.go
@@ -9,9 +9,9 @@ import (
 	"golang.org/x/crypto/sha3"
 )
 
-func Auth(auth *types.Auth) bitfield.Uint64 {
+func Auth(auth *types.Auth) (bitfield.Uint64, int64) {
 	if auth == nil {
-		return 0
+		return 0, -1
 	}
 	rid, id, dig, ent, req := auth.RowID, auth.ID, auth.Digest, auth.Entropy, auth.Request
 	q := query.Query{DB: DB()}
@@ -26,38 +26,38 @@ func Auth(auth *types.Auth) bitfield.Uint64 {
 		q.Query(id)
 	}
 	if !q.OK() {
-		return 0
+		return 0, -1
 	}
 	if !q.NextOrClose() {
-		return 0
+		return 0, -1
 	}
 	var key []byte
 	var qFlags uint64
 	q.ScanClose(&key, &qFlags)
 	if !q.OK() {
-		return 0
+		return 0, -1
 	}
 	if hex.DecodedLen(dig) != 32 { // Anti-DoS
-		return 0
+		return 0, -1
 	}
 	digBytes, err := hex.DecodeString(dig)
 	if err != nil {
-		return 0
+		return 0, -1
 	}
 	if hex.DecodedLen(ent) != 32 { // Anti-DoS
-		return 0
+		return 0, -1
 	}
 	entBytes, err := hex.DecodeString(ent)
 	if err != nil {
-		return 0
+		return 0, -1
 	}
 	buf := append(entBytes, key)
 	actual := sha3.Sum256(buf)
 	if len(actual) != len(digBytes) { // Anti-DoS
-		return 0
+		return 0, -1
 	}
 	if actual != digBytes {
-		return 0
+		return 0, -1
 	}
-	return qFlags & req
+	return qFlags & req, id
 }
